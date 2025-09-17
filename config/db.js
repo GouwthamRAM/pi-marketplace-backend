82030ce46bb4a439268dc37192e7915b25fb0f31
charu <charulatha.sanmathi.cd@gmail.com>
2025-09-17 21:51:26 +0100

    Trigger Railway deployment

--- config/db.js
+++ config/db.js
@@ -1,15 +1,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false,
-  }
-);
+    protocol: 'postgres',
+    dialectOptions: { ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false },
+    logging: false
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      logging: false
+    }
+  );
+}
 
 module.exports = sequelize;
